60d795b17d47c64a271df4e200108e4f2f27dcf0
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-06-19 17:39:34 -0300

    style: :lipstick: Adicionando váriaveis do tema

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  background: "#fef6e4", // Cor de fundo principal
+  headline: "#001858", // Cor para títulos principais
+  paragraph: "#172c66", // Cor para textos de apoio/descrições
+  button: "#f582ae", // Cor de fundo do botão principal
+  buttonText: "#001858", // Cor do texto do botão principal
+
+  // Cores de ilustração
+  stroke: "#001858",
+  illustration: {
+    main: "#f3d2c1",
+    highlight: "#fef6e4",
+    secondary: "#8bd3dd",
+    tertiary: "#f582ae",
+  },
+};
