ea21aef2e36c0e5d3da06a3a89b2350bbdb14b3d
ArtmdX <arthur.mdx1234@gmail.com>
2025-06-21 03:41:53 -0300

    feat: Assegurando chave da API

---
+++ app.config.js
@@ -0,0 +1,41 @@
+import 'dotenv/config'; // Carrega as variáveis do arquivo .env
+
+export default {
+  expo: {
+    name: 'Maia',
+    slug: 'maia-mobile-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/ic_launcher.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#FEF9FA'
+    },
+    plugins: ['expo-font'],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#F5C6D6'
+      },
+      package: 'com.artmdx.Maia',
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_Maps_API_KEY
+        }
+      }
+    },
+    web: {
+      favicon: './assets/favicon.png'
+    },
+    extra: {
+      eas: {
+        projectId: 'd5e63318-eeca-4cf3-97c8-18ca4fbc4ab9'
+      }
+    }
+  }
+};
